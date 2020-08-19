c59cbb58c0be07d08a606eeae12433deb3e8820e
varant-t <vv_titizian@hotmail.ca>
2020-08-19 01:32:46 -0400

    Lab 5

---
+++ W05_AI_Animation/Assets/Scripts/Character.cs
@@ -0,0 +1,192 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// The GameObject is made to bounce using the space key.
+// Also the GameOject can be moved forward/backward and left/right.
+// Add a Quad to the scene so this GameObject can collider with a floor.
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+public class Character : MonoBehaviour
+{
+    public float speed;
+    public float jumpSpeed;
+    public float gravity;
+
+    Vector3 moveDirection = Vector3.zero;
+    public CharacterController controller;
+
+    public Transform thingToLookFrom;
+    public float lookDistance;
+
+    public Rigidbody projectilePrefab;
+    public Transform projectileSpawnPoint;
+    public float projectileForce;
+
+    Animator animator;
+
+    Enemy en;
+    void Start()
+    {
+        en = GetComponent<Enemy>();
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+        if (speed <= 0)
+        {
+            speed = 6.0f;
+
+            Debug.Log("Speed not set on " + name + ". Defaulting to " + speed);
+        }
+
+        if (jumpSpeed <= 0)
+        {
+            jumpSpeed = 8.0f;
+
+            Debug.Log("JumpSpeed not set on " + name + ". Defaulting to " + jumpSpeed);
+        }
+
+        if (gravity <= 0)
+        {
+            gravity = 20.0f;
+
+            Debug.Log("Gravity not set on " + name + ". Defaulting to " + gravity);
+        }
+
+        if (lookDistance <= 0)
+        {
+            lookDistance = 10.0f;
+
+            Debug.Log("LookDistance not set on " + name + ". Defaulting to " + lookDistance);
+        }
+
+        if (projectileForce <= 0)
+        {
+            projectileForce = 10.0f;
+
+            Debug.Log("ProjectileForce not set on " + name + ". Defaulting to " + projectileForce);
+        }
+
+        // let the gameObject fall down
+        //gameObject.transform.position = new Vector3(0, 5, 0);
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+
+        // Used for the Raycast to store information about what it collides with
+        RaycastHit hit;
+
+        // If there is no thingToLookFrom, default to Character
+        if(!thingToLookFrom)
+        {
+            Debug.DrawRay(transform.position, transform.forward * lookDistance, Color.red);
+
+            if(Physics.Raycast(transform.position, transform.forward, out hit, lookDistance))
+            {
+                Debug.Log("Raycast: " + hit.collider.gameObject.name);      
+            }
+        }
+        else
+        {
+            Debug.DrawRay(thingToLookFrom.position, thingToLookFrom.forward * lookDistance, Color.red);
+
+            if (Physics.Raycast(thingToLookFrom.position, thingToLookFrom.forward, out hit, lookDistance))
+            {
+                Debug.Log("Raycast: " + hit.collider.gameObject.name);
+            }
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            //fire();
+
+            // Use Animation Event to create projectile
+            animator.SetTrigger("Attack");
+            
+           
+        }
+
+        animator.SetFloat("Speed", transform.TransformDirection(controller.velocity).z);
+        animator.SetBool("IsGrounded", controller.isGrounded);
+    }
+
+    void fire()
+    {
+        Debug.Log("Pew Pew");
+
+        Rigidbody temp = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
+
+        temp.AddForce(projectileSpawnPoint.forward * projectileForce, ForceMode.Impulse);
+    }
+
+    // Usage Rules:
+    // - Both GameObjects must have Colliders
+    // - One or both GameObjects must have a Rigidbody
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("OnCollisionEnter: " + collision.gameObject.name);
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("OnCollisionExit: " + collision.gameObject.name);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("OnCollisionStay: " + collision.gameObject.name);
+    }
+
+    // Usage Rules:
+    // - GameObject must have CharacterController
+    // - Other GameObject must have a Collider
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("OnControllerColliderHit: " + hit.gameObject.name);
+    }
+
+    // Usage Rules:
+    // - GameObject must have Collider marked as "IsTrigger"
+    // - One or both GameObjects must have a Collider
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter: " + other.gameObject.name);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit: " + other.gameObject.name);
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        Debug.Log("OnTriggerStay: " + other.gameObject.name);
+    }
+
+    public void HitAnimation()
+    {
+        
+    }
+
+}
---
+++ W05_AI_Animation/Assets/Scripts/Enemy.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))] 
+public class Enemy : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator animator;
+    Rigidbody rigidbody;
+
+    public Item itemPrefab;
+    public Transform itemSpawnPoint;
+    
+    
+    public GameObject target;
+
+    public bool autoGenPath;
+    public string pathName;
+
+    public GameObject[] path;
+    public int pathIndex;
+
+    enum EnemyState{Chase, Patrol}; //lets us choose the state of enemy
+    [SerializeField] EnemyState state;
+
+    enum PatrolType { DistanceBased, TriggerBased};
+    [SerializeField] PatrolType patrolType;
+
+    public float distanceToNextNode;
+
+     Character character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+       character = GetComponent<Character>();
+       
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+
+        animator.applyRootMotion = false;
+        rigidbody.isKinematic = true;
+        rigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic; // checks for contionus motion , if ai stops moving use this
+
+        if(string.IsNullOrEmpty(pathName))
+        {
+            pathName = "PathNode";
+        }
+
+        if(autoGenPath)
+        {
+            path = GameObject.FindGameObjectsWithTag(pathName); // is autogenpath is selected, puts them all in the array for you.
+        }
+
+        if(distanceToNextNode <= 0)
+        {
+            distanceToNextNode = 1.0f;
+        }
+
+        if(state == EnemyState.Chase)
+        {
+            target = GameObject.FindWithTag("Player");
+           
+        }
+        else if (state == EnemyState.Patrol)
+        {
+            if (path.Length > 0)
+            {
+                target = path[pathIndex];
+                
+            }
+        }
+
+        if(target)
+        {
+            agent.SetDestination(target.transform.position);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(state == EnemyState.Patrol && patrolType == PatrolType.DistanceBased)
+        {
+            Debug.DrawLine(transform.position, target.transform.position, Color.green);
+            // if (Vector3.Distance(transform.position, target.transform.position) < distanceToNextNode) (1 way of finding pathing)
+            //if (agent.remainingDistance < distanceToNextNode) (2nd way of finding pathing) 
+            if (Vector3.Distance(transform.position, target.transform.position) < distanceToNextNode)
+            {
+                if(path.Length >0)
+                {
+                    pathIndex++;
+                    pathIndex %= path.Length;
+                    target = path[pathIndex];
+                }
+            }
+        }
+
+        if (target)
+        {
+            agent.SetDestination(target.transform.position);
+           
+        }
+
+        animator.SetBool("IsGrounded", !agent.isOnOffMeshLink); // means were jumping somewhere
+        animator.SetFloat("Speed", transform.InverseTransformDirection(agent.velocity).z);
+    }
+
+    //Add COllider to Enemy
+    // Add Rigidbody to Enemy
+    // - Rigidbody shoudl be set to IsKiematic
+
+    private void OnTriggerEnter(Collider other)
+    {
+      
+        //Instantiate(itemPrefab, itemSpawnPoint.position, itemSpawnPoint.rotation);
+        if (other.gameObject.tag == "Player")
+        {
+          
+           animator.SetBool("IsDead", true);
+            
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+       
+    }
+}
